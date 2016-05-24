77cf84813288ae27581d6d5b8d4265a73b4dc0e5
Li Zonglei <zonglei.li@dewmobile.net>
2016-05-24 15:04:25 +0800

    should choose a calendar before 'pin' calendar event.

--- app/src/main/java/lz/mylife/HomeActivity.java
+++ app/src/main/java/lz/mylife/HomeActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
@@ -131,4 +132,11 @@ public class HomeActivity extends AppCompatActivity {
             }
         }
     };
+
+    private class CalendarListTask extends AsyncTask<Void, Void, String[]> {
+        @Override
+        protected String[] doInBackground(Void... params) {
+            return null;
+        }
+    }
 }
