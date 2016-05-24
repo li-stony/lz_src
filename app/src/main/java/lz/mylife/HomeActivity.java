7810956e8c529d38f0ab88a1cee049f484a56786
Li Zonglei <zonglei.li@dewmobile.net>
2016-05-24 14:39:04 +0800

    make it run well on android 6.x

--- app/src/main/java/lz/mylife/HomeActivity.java
+++ app/src/main/java/lz/mylife/HomeActivity.java
@@ -1,14 +1,18 @@
 package lz.mylife;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import lz.mylife.cal.CalendarService;
@@ -26,6 +30,7 @@ public class HomeActivity extends AppCompatActivity {
     private LocationService.LzLocation location;
     private WeatherService.LzWeatherLive weatherLive;
     private WeatherService.LzWeatherDay weatherDay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,11 +45,51 @@ public class HomeActivity extends AppCompatActivity {
         filter.addAction(WeatherService.ACTION_PREDICT_WEATHER_GOT);
         LocalBroadcastManager.getInstance(this).registerReceiver(receiver, filter);
 
+        initPermissions();
     }
 
+    private void initPermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{
+                        Manifest.permission.WRITE_CALENDAR,
+                        Manifest.permission.INTERNET,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_NETWORK_STATE,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.READ_PHONE_STATE,
+                        Manifest.permission.ACCESS_WIFI_STATE,
+                        Manifest.permission.CHANGE_CONFIGURATION,
+                        Manifest.permission.WRITE_SETTINGS,
+                        Manifest.permission.CHANGE_WIFI_STATE,
+                        Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS
+                },
+                1);
+    }
+    boolean granted = false;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    granted = true;
+                    LocationService.start(this.getApplicationContext());
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+        }
+    }
     public void onStart() {
         super.onStart();
-        LocationService.start(this.getApplicationContext());
+        if(granted) {
+            LocationService.start(this.getApplicationContext());
+        }
     }
     @Override
     public void onStop() {
