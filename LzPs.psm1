44c309e4d61cc5529644ccf9077a029b6d85d964
Li Zonglei <zonglei.li@dewmobile.net>
2016-07-11 11:41:57 +0800

    init

---
+++ LzPs.psm1
@@ -0,0 +1,39 @@
+﻿$LzDef = @"
+namespace Lz.PowerShell {
+    using System.Runtime.InteropServices; 
+    public class MkLink {
+        [DllImport("kernel32.dll")]
+        public static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, int dwFlags);
+    }
+}
+"@
+
+Add-Type -TypeDefinition $LzDef
+
+function New-Link {
+    param (
+        [string]$Link,
+        [string]$Target,
+        [switch]$Directory
+
+    )
+    
+    # Write-Host "hello world"
+    $flag = 0
+    if($Directory){
+        $flag = 1
+    }
+    if([string]::IsNullOrEmpty($Link) -or [string]::IsNullOrEmpty($Target)) {
+        Write-Host -ForegroundColor Red "New-Link -Link path -Target path [-Directory]"
+        return $false
+    }
+        
+    $linkPath = [System.IO.Path]::GetFullPath($Link)
+    $targetPath = [System.IO.Path]::GetFullPath($Target)
+    $result = [Lz.PowerShell.MkLink]::CreateSymbolicLink($linkPath.Path, $targetPath.Path, $flag)
+    return $result
+
+
+    
+}
+Export-ModuleMember -Function New-Link
