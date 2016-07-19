46e6933b3a252f713a65b8d9d19afd9dd67c7e6b
Li Zonglei <zonglei.li@dewmobile.net>
2016-07-19 10:56:59 +0800

    fix bug that can't draw a correct size mark bitmap

--- lz_scripts/watermark/watermark.ps1
+++ lz_scripts/watermark/watermark.ps1
@@ -15,7 +15,7 @@ $minLen = $img.Width
 if($minLen -gt $img.Height) {
     $minLen = $img.Height
 }
-$minLen = $minLen / 2;
+$minLen = $minLen / 2.5;
 [Int32]$minLen = [System.Math]::Floor($minLen)
 #Get-Member -InputObject $minLen
 #Write-Host $minLen
@@ -23,22 +23,27 @@ $bmp = [System.Drawing.Bitmap]::new($minLen, $minLen)
 
 # draw
 $g = [System.Drawing.Graphics]::FromImage($bmp)
-$textMargin = $minLen / 4
+$textMargin = $minLen / 6
 $charSize = ($minLen - 2*$textMargin) / $Mark.Length
 [Int32]$charSize = [Math]::Floor($charSize)
+[Int32]$maxSize = ($minLen - 2*$textMargin) /2
+if($charSize -gt $maxSize) {
+    $charSize = $maxSize
+}
 $g.TranslateTransform($minLen/2, $minLen/2)
 $g.RotateTransform(45)
 $g.TranslateTransform(-$minLen/2, -$minLen/2)
 # draw background
 $g.FillRectangle([System.Drawing.SolidBrush]::new([System.Drawing.Color]::Transparent), 0,0,$minLen,$minLen)
 # draw text background
-$g.FillRectangle([System.Drawing.SolidBrush]::new([System.Drawing.Color]::OrangeRed), 0, $minLen/2-$charSize, $minLen, $charSize)
+$g.FillRectangle([System.Drawing.SolidBrush]::new([System.Drawing.Color]::OrangeRed), 0, $minLen/2-$maxSize, $minLen, $maxSize)
 
 $fontSet = [System.Drawing.Font]::new([System.Drawing.FontFamily]::new("Arial"), $charSize, [System.Drawing.GraphicsUnit]::Pixel)
 $strFmt = [System.Drawing.StringFormat]::new()
 $strFmt.Alignment = [System.Drawing.StringAlignment]::Center
+$strFmt.LineAlignment = [System.Drawing.StringAlignment]::Center
 $brush = [System.Drawing.SolidBrush]::new([System.Drawing.Color]::White)
-$rect = [System.Drawing.RectangleF]::new(0, $minLen/2 - $charSize, $minLen, $charSize)
+$rect = [System.Drawing.RectangleF]::new(0, $minLen/2 - $maxSize, $minLen, $maxSize)
 
 $g.DrawString($Mark, $fontSet, $brush, $rect, $strFmt)
 $g.Flush()
@@ -48,12 +53,13 @@ $bmp.Save($folderPath+"\mark.jpg")
 # draw the bmp to target image file
 $g = [System.Drawing.Graphics]::FromImage($img)
 $offset = $minLen * 0.2 * 0.8
+
 $x = $img.Width + $offset - $minLen
 $y = 0 - $offset
-# Write-Host $img.Width $minLen $x $y
+Write-Host $img.Width $minLen $x $y
 [Int32]$x = [Math]::Floor($x)
 [Int32]$y = [Math]::Floor($y)
-$g.DrawImage($bmp, $x, $y)
+$g.DrawImage($bmp, $x, $y, $bmp.Width, $bmp.Height)
 $g.Flush()
 $g.Dispose()
 $img.Save($folderPath + "\result.jpg")
