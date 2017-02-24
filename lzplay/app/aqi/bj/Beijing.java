38197a5231308432f4c003494c82ca2f9f376937
Li Zonglei <zonglei.li@dewmobile.net>
2017-02-24 16:14:59 +0800

    refact

--- lzplay/app/aqi/bj/Beijing.java
+++ lzplay/app/aqi/bj/Beijing.java
@@ -1,7 +1,13 @@
 package aqi.bj;
 
+import akka.actor.UntypedActor;
+
 /**
  * Created by cussyou on 17-2-16.
  */
-public class Beijing {
+public class Beijing extends UntypedActor {
+    @Override
+    public void onReceive(Object message) throws Throwable {
+
+    }
 }
