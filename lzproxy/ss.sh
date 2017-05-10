ed0689abd7b74a9bda9e0c3bd0ddabafdc37a7ff
Li Zonglei <zonglei.li@dewmobile.net>
2017-05-10 17:18:07 +0800

    auto startup sslocal and polipo

---
+++ lzproxy/ss.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+unset http_proxy
+
+ss=$(ps -A|grep sslocal)
+if [ -n "$ss" ] 
+then
+    echo "existed: $ss"
+else
+    nohup ~/.local/bin/sslocal -c ~/.ssconfig.json &> ~/logs/ss.log &
+fi
+
+po=$(ps -A | grep polipo)
+if [ -n "$po" ]
+then
+    echo "existed: $po"
+else
+    polipo socksParentProxy=localhost:1080 logFile=~/logs/polipo.log &    
+fi
+
+export http_proxy='http://localhost:8123'
+echo $http_proxy
+    
+# shouldn't run ss.sh directly.
+# instead, like below.
+# source /path/ss.sh
