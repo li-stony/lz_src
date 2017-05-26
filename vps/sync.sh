b65646bd3744e2422c1c0d1c883fb61208af91e4
Li Zonglei <zonglei.li@dewmobile.net>
2017-05-26 16:57:32 +0800

    fix sync.sh bugs

--- vps/sync.sh
+++ vps/sync.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+rsync -r -v /home/cussyou/lz_datas/notes root@lizl.me:/root/ &> /home/cussyou/logs/sync.log
 
+rsync -r -v root@lizl.me:/root/notes/00todo /home/cussyou/lz_datas/notes/ >> /home/cussyou/logs/sync.log 2>&1
