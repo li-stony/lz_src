189161a3d7d61e47568969cf9f195e39b697ec40
Li Zonglei <zonglei.li@dewmobile.net>
2017-05-27 14:16:33 +0800

    change the sync way

--- vps/sync.sh
+++ vps/sync.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-rsync -r -v /home/cussyou/lz_datas/notes root@lizl.me:/root/ &> /home/cussyou/logs/sync.log
+d=$(date +"%Y-%m-%d %H:%M")
+echo "$d start" > /home/cussyou/logs/sync.log
 
-rsync -r -v root@lizl.me:/root/notes/00todo /home/cussyou/lz_datas/notes/ >> /home/cussyou/logs/sync.log 2>&1
+# backup all local data to vps
+# and remove deleted files
+rsync -r -v --delete /home/cussyou/lz_datas/notes root@lizl.me:/root/ >> /home/cussyou/logs/sync.log 2>&1
+
+# sync files added on vps directly to local machine
+rsync -r -v root@lizl.me:/root/notes /home/cussyou/lz_datas/ >> /home/cussyou/logs/sync.log 2>&1
+
+d=$(date +"%Y-%m-%d %H:%M")
+echo "$d end" >> /home/cussyou/logs/sync.log
