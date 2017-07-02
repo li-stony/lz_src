e12f16b333f27110e5cdceac31353329208c2c0b
Li Zonglei <zonglei.li@dewmobile.net>
2017-07-02 15:22:29 +0800

    add export

--- gg/gupload.sh
+++ gg/gupload.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export http_proxy='http://127.0.0.1:8123'
+export https_proxy='http://127.0.0.1:8123'
+
 gdrive sync list --no-header | while read line
 do
     item=$(echo $line | awk '{print "/home/cussyou/" $2 " " $1}')
@@ -20,7 +23,7 @@ do
 	    num=$(( num + 1 ))
 	fi
 	echo "error times: $num"
-	if [ $num -gt 5 ]
+	if [ $num -gt 10 ]
 	then
 	    break
 	fi
