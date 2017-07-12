a873b14365d495740a4ae47eb7eedb255ee4de04
Li Zonglei <zonglei.li@dewmobile.net>
2017-07-12 10:48:18 +0800

    auto upload

--- gg/gupload.sh
+++ gg/gupload.sh
@@ -3,16 +3,19 @@
 export http_proxy='http://127.0.0.1:8123'
 export https_proxy='http://127.0.0.1:8123'
 
-gdrive sync list --no-header | while read line
+
+# sync picture only the current year
+year=$(date +"%Y")
+gdrive sync list | grep -E "[[:space:]]+$year[[:space:]]+" | while read line
 do
-    item=$(echo $line | awk '{print "/home/cussyou/" $2 " " $1}')
+    item=$(echo $line | awk '{print "/home/cussyou/lz-picture/" $2 " " $1}')
     echo "$item"
     code=1
     # loop until end
     num=0
     while [ $code -ne 0 ]
     do
-	echo "$item" | xargs -n 2 gdrive sync upload  
+	echo "$item" | xargs -n 2 gdrive sync upload --chunksize 4096 --timeout 150
 	code=$?
 	echo "sync exit: $code"
 	sleep 10
@@ -21,12 +24,46 @@ do
 	if [ $code -ne 0 ]
 	then
 	    num=$(( num + 1 ))
+	else
+	    num=0
 	fi
 	echo "error times: $num"
-	if [ $num -gt 10 ]
+	if [ $num -gt 20 ]
+	then
+	    break
+	fi
+    done
+done
+
+# sync others
+gdrive sync list --no-header |sed -E '/[[:space:]]+[0-9]{4,4}[[:space:]]+/d' | sed -E '/lz-picture/d'| while read line
+do
+    item=$(echo $line | awk '{print "/home/cussyou/" $2 " " $1}')
+    
+    echo "$item"
+    code=1
+    # loop until end
+    num=0
+    while [ $code -ne 0 ]
+    do
+	echo "$item" | xargs -n 2 gdrive sync upload --chunksize 4096 --timeout 150
+	code=$?
+	echo "sync exit: $code"
+	sleep 10
+
+	# 
+	if [ $code -ne 0 ]
+	then
+	    num=$(( num + 1 ))
+	else
+	    num=0
+	fi
+	echo "error times: $num"
+	if [ $num -gt 20 ]
 	then
 	    break
 	fi
     done    
 done
+
 
