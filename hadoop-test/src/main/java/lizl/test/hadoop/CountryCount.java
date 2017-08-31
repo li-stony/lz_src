56860560e835ac4f136f0c05118a29f227d75f2e
Li Zonglei <zonglei.li@dewmobile.net>
2017-08-31 17:10:08 +0800

    add Country counting

---
+++ hadoop-test/src/main/java/lizl/test/hadoop/CountryCount.java
@@ -0,0 +1,43 @@
+package lizl.test.hadoop;
+
+import lizl.test.hadoop.country.CountryMap;
+import lizl.test.hadoop.ip.IpMap;
+import lizl.test.hadoop.ip.IpReduce;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class CountryCount {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration conf = new Configuration();
+        //GenericOptionsParser optionsParser = new GenericOptionsParser(conf, args);
+        if(args.length != 2){
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<args.length;i++) {
+                sb.append(args[i]).append(" ");
+            }
+            System.out.println("error: "+sb.toString());
+            System.err.println("usage: CountryCount <in> <out>");
+            System.exit(2);
+        }
+
+        Job job = Job.getInstance(conf, "country_count");
+        job.setJarByClass(CountryCount.class);
+        job.setMapperClass(CountryMap.class);
+        job.setReducerClass(IpReduce.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true)?0:1);
+    }
+}
