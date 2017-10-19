a3b7cc2d934accd8808a25f63f6e25480741a5fb
Li Zonglei <zonglei.li@dewmobile.net>
2017-10-19 14:54:21 +0800

    needn't  push to remote now

--- disk/sync_datas.sh
+++ disk/sync_datas.sh
@@ -3,7 +3,7 @@ t=`date "+%Y%m%d %H:%M"`
 echo "-- $t --"
 h=`hostname`
 cd /home/cussyou/lz-datas
-git pull origin master
+#git pull origin master
 git add .
 git commit -m "Auto commit from aws at $t on $h"
-git push origin master
+#git push origin master
