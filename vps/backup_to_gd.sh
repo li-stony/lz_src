85a10def147ddb2206960e4e4ce4665bce7ef55e
Li Zonglei <zonglei.li@dewmobile.net>
2017-10-23 13:49:39 +0800

    checksum for text files, and size-only for binary files

--- vps/backup_to_gd.sh
+++ vps/backup_to_gd.sh
@@ -7,12 +7,29 @@ echo "== $d start =="
 root="/home/cussyou"
 backdir="cussyou@gc.lizl.me:/home/cussyou/google_drive"
 
-items=('lz-datas' 'lz-src' 'lz-picture' 'lz-video' 'lz-books' 'lz-music')
+# backup my notes and profiles
+items=('lz-datas lz-src')
 echo ${items[@]}
 for i in ${items[@]}
 do
     echo $i
-    rsync -v -t -r --size-only "$root/$i" "$backdir" 
+    # rsync -v -r --size-only --progress  "$root/$i" "$backdir" 
+    # rsync -t -v -r --progress  "$root/$i" "$backdir" 
+    rsync -v -r --checksum --progress "$root/$i" "$backdir"
+done
+
+d=$(date +"%Y-%m-%d %H:%M")
+echo "== $d =="
+
+# backup my binary files
+items=('lz-picture' 'lz-video' 'lz-books' 'lz-music')
+echo ${items[@]}
+for i in ${items[@]}
+do
+    echo $i
+    rsync -v -r --size-only --progress  "$root/$i" "$backdir" 
+    # rsync -t -v -r --progress  "$root/$i" "$backdir" 
+    # rsync -v -r --checksum --progress "$root/$i" "$backdir"
 done
 
 d=$(date +"%Y-%m-%d %H:%M")
