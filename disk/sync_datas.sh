40b2f0512799c5a1736b1f24a0df10d26adc155d
Li Zonglei <zonglei.li@dewmobile.net>
2017-10-23 13:50:59 +0800

    fixed the hostname

--- disk/sync_datas.sh
+++ disk/sync_datas.sh
@@ -5,5 +5,5 @@ h=`hostname`
 cd /home/cussyou/lz-datas
 #git pull origin master
 git add .
-git commit -m "Auto commit from aws at $t on $h"
+git commit -m "Auto commit on $h at $t "
 #git push origin master
