df998bdc4684b5ca0b358ec0b8fbc52d4dbd8462
lizl@ge63 <cussyou@gmail.com>
2018-03-06 10:33:24 +0800

    not all datas in home path

--- disk/backup.sh
+++ disk/backup.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+set -x
 
 d=$(date +"%Y-%m-%d %H:%M")
 echo "== $d start ==" 
-
 root="/home/cussyou"
-backdir="/media/cussyou/back"
+root2="/media/cussyou/datas"
+backdir="/media/cussyou/Datas/lizl/backup"
 
-items=('lz-datas' 'lz-profile' 'lz-files' 'lz-src' 'lz-picture' 'lz-video' 'lz-pri' 'lz-books' 'lz-music')
+items=('lz-datas' 'lz-src')
 echo ${items[@]}
 for i in ${items[@]}
 do
     echo $i
     rsync -v -t -r "$root/$i" "$backdir" 
+done
+
+items=('lz-picture' 'lz-video' 'lz-pri' 'lz-books' 'lz-music')
+echo ${items[@]}
+for i in ${items[@]}
+do
+    echo $i
+    rsync -v -t -r "$root2/$i" "$backdir" 
 done
 
 d=$(date +"%Y-%m-%d %H:%M")
