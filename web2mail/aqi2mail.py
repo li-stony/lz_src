39514016b66d8fd2eadbd3bb82f5bae5a3cb5d13
lizl@ge63 <cussyou@gmail.com>
2018-04-10 10:14:03 +0800

    more logs, and retry after error

--- web2mail/aqi2mail.py
+++ web2mail/aqi2mail.py
@@ -4,6 +4,7 @@ import json
 import urllib
 import urllib.request
 import re
+import time
 
 def get_data():
     # beijing url
@@ -19,39 +20,49 @@ def get_data():
     res = urllib.request.urlopen(req, timeout=600)
     body = res.read()
     s = body.decode('utf8')
-    json_data = json.loads(s)
-    out = json.dumps(json_data, indent=4)
-    # print(out)
-    # city
-    obs = json_data['rxs']['obs']
-    msg = obs['msg']
-    city = msg['city']['name']
-    city_url = msg['city']['url']
-    aqi = msg['aqi']
+    
+    try:
+        json_data = json.loads(s)
+        out = json.dumps(json_data, indent=4)
+        # print(out)
+        # city
+        obs = json_data['rxs']['obs']
+        msg = obs[0]['msg']
+        city = msg['city']['name']
+        city_url = msg['city']['url']
+        aqi = msg['aqi']
 
-    iaqi = msg['iaqi']
-    pm25 = None
-    pm10 = None
-    for item in iaqi:
-        if item['p'] == 'pm25':
-            print(item)
-            pm25 = item['v']
-        elif item['p'] == 'pm10':
-            print(item)
-            pm10 = item['v']
-    # update_time = msg['time']['s']['cn']['time']
-    update_time = msg['time']['s']['zh-CN']['time']
-    # collection
-    title = '{} {} AQI {}'.format(city, update_time, aqi)
-    body = '{} <br>\nPM2.5: {}<br>\nPM10: {}<br>\n'.format(title, pm25, pm10)
+        iaqi = msg['iaqi']
+        pm25 = None
+        pm10 = None
+        for item in iaqi:
+            if item['p'] == 'pm25':
+                print(item)
+                pm25 = item['v']
+            elif item['p'] == 'pm10':
+                print(item)
+                pm10 = item['v']
+        # update_time = msg['time']['s']['cn']['time']
+        update_time = msg['time']['s']['zh-CN']['time']
+        # collection
+        title = '{} {} AQI {}'.format(city, update_time, aqi)
+        body = '{} <br>\nPM2.5: {}<br>\nPM10: {}<br>\n'.format(title, pm25, pm10)
 
-    return title,body
-
+        return title,body
+    except:
+        print(s)
+        return None,None
 
 def main():
     print(datetime.datetime.now(), 'start aqi')
     # get json data from aqi
-    title,body = get_data()
+    for i in range(0,3):
+        title,body = get_data()
+        if title is None:
+            print('failed. try later...')
+            time.sleep(30)
+        else:
+            break
     print(title)
     print(body)
     print(datetime.datetime.now(), 'end aqi')
